c5646a1f45c5cf9d6470e15f4976638514f27a9e
mjancic <mitjajancic@gmail.com>
2021-07-17 09:18:21 +0200

    Deployment script fixed.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/env sh
-# abort on errors
-set -e
-# build
-npm run build
-# navigate into the build output directory
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:distractor/metuljmania-application-gui.git master:gh-pages
+npm run build &&
+cd dist &&
+git init &&
+git add -A &&
+git commit -m 'Deploy.' &&
+git push -f git@github.com:distractor/metuljmania-application-gui.git master:gh-pages &&
 cd -
