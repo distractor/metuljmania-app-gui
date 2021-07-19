2485fef3e79117520dbadce3cf35b880bbeb120a
mjancic <mitjajancic@gmail.com>
2021-07-19 17:24:15 +0200

    Deployment file modified from CRLF to LF.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-npm run build &&
-cd dist &&
-git init &&
-git add -A &&
-git commit --allow-empty -m 'Deploy.' &&
-git push -f git@github.com:distractor/metuljmania-application-gui.git master:gh-pages &&
+npm run build  
+cd dist  
+git init  
+git add -A  
+git commit --allow-empty -m 'Deploy.'  
+git push -f git@github.com:distractor/metuljmania-application-gui.git master:gh-pages  
 cd ..
